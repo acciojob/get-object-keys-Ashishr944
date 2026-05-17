a936e85f69de07d459e38643768a04d10ace1fd3
Ashish Rokade <ashishrokade944@gmail.com>
2026-05-17 21:36:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// Create student object
+const student = {
+    name: "John"
+};
+
+// Function to get all object keys
+function getKeys(obj) {
+    return Object.keys(obj);
+}
